c837f747773fb7add7acdbaa4317d55f14cc1e6f
Adiya-32 <adiya.khasan@mail.ru>
2026-03-01 11:38:29 +0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -129,10 +129,9 @@ async function startServer() {
     });
   }
 
-  const PORT = process.env.PORT || 3000;
 
-app.listen(Number(process.env.PORT) || 3000, "0.0.0.0", () => {
-  console.log("Server is running on port 3000");
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
 }
 
